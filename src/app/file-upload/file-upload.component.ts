9b77bf65e8a9de7188837f944548ed830d579c38
Ben-Malik <benmaliktchamalam@gmail.com>
2021-08-27 00:08:55 +0300

    Refactored the project Added file size validations

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,9 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {tap} from 'rxjs/operators';
-import {HttpClient, HttpEventType, HttpResponse} from '@angular/common/http';
-import {FileService} from '../service';
+import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { HttpClient, HttpEventType, HttpResponse } from '@angular/common/http';
+import { FileService } from '../service';
 import { UploadedFile } from '../file.model';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+const MAX_FILE_SIZE = 20000000; //20M
+const FORBIDDEN_FILES = ["js", "html", "php", "java"];
 
 @Component({
   selector: 'app-multi-file-upload',
@@ -16,13 +19,12 @@ export class FileUploadComponent implements OnInit {
   selectedFiles!: FileList;
   uploadedFiles: UploadedFile[] = [];
   showProgress = false;
- 
+  isFileValid = true;
 
   constructor(private http: HttpClient, private fileService: FileService, private snackBar: MatSnackBar) {
   }
- 
-  ngOnInit(): void {
-  }
+
+  ngOnInit(): void {  }
 
   // Selected file is stored into selectedFiles.
   selectFile(event: any) {
@@ -36,26 +38,34 @@ export class FileUploadComponent implements OnInit {
     Array.from(this.selectedFiles).forEach(file => {
       const uploadedFile = new UploadedFile();
       uploadedFile.fileName = file.name;
-      this.uploadedFiles.push(uploadedFile);
-      this.fileService.uploadFileToDB(file)
-        .pipe(tap(event => {
-          if (event.type === HttpEventType.UploadProgress) {
-            const totalNum = event!.total;
-            if (totalNum != null) {
-              this.loaded = Math.round(100 * event.loaded / totalNum);
-              uploadedFile.progress = this.loaded;
-            } else {
-              return;
+      uploadedFile.fileSize = file.size;
+      if (uploadedFile.fileSize <= MAX_FILE_SIZE) {
+        uploadedFile.isFileValid = true;
+        this.uploadedFiles.push(uploadedFile);
+        this.fileService.uploadFileToDB(file)
+          .pipe(tap(event => {
+            if (event.type === HttpEventType.UploadProgress) {
+              const totalNum = event!.total;
+              if (totalNum != null) {
+                this.loaded = Math.round(100 * event.loaded / totalNum);
+                uploadedFile.progress = this.loaded;
+
+              } else {
+                return;
+              }
             }
-          }
-        })).subscribe((event: any) => {
-        if (event instanceof HttpResponse) {
-          if (this.selectedFiles.item(this.selectedFiles.length - 1) === file) {
-            // Invokes fetchFileNames() when last file in the list is uploaded.
-            this.fileService.fetchFiles();
-          }
-        }
-      });
+          })).subscribe((event: any) => {
+            if (event instanceof HttpResponse) {
+              if (this.selectedFiles.item(this.selectedFiles.length - 1) === file) {
+                // Invokes fetchFileNames() when last file in the list is uploaded.
+                this.fileService.fetchFiles();
+              }
+            }
+          });
+      } else {
+        uploadedFile.isFileValid = false;
+        this.isFileValid = false;
+      }
     });
   }
 
--- src/app/file.model.ts
+++ src/app/file.model.ts
@@ -2,5 +2,6 @@ export class UploadedFile {
 
     fileName: string | undefined;
     progress: number | undefined;
-    
+    fileSize: number | undefined;
+    isFileValid: boolean = false;
 }
